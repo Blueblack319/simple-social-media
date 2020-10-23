8076b496e02ff70239943d97b83b8b3606fa1d30
Yang Jae Hoon <didwogns789@naver.com>
2020-10-23 10:29:31 +0900

    Delete comment: Finishing up the server

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-import { UserInputError } from "apollo-server";
+import { AuthenticationError, UserInputError } from "apollo-server";
 
 import checkAuth from "../../utils/check-auth";
 import Post from "../../models/Post";
@@ -23,6 +23,23 @@ const commentResolvers = {
         });
         await post.save();
         return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+    deleteComment: async (_, { postId, commentId }, context) => {
+      const { userName } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        const commentIndex = post.comments.findIndex(
+          (comment) => comment.id === commentId
+        );
+        if (post.comments[commentIndex].userName === userName) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
       } else {
         throw new UserInputError("Post not found");
       }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -36,6 +36,7 @@ const typeDefs = gql`
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
     createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
   }
 `;
 
