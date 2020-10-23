29a069b5468307286ff3d895a5145a59ad646efa
Yang Jae Hoon <didwogns789@naver.com>
2020-10-23 14:34:42 +0900

    Semantic UI & Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
 import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World!</h1>
+      <Container>
+        <Router>
+          <MenuBar />
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+            <Route exac path='/' component={Home} />
+          </Switch>
+        </Router>
+      </Container>
     </div>
   );
 }
